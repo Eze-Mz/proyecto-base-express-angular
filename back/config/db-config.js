48d2bfed09c783cb57990795a32d79906cb20f18
Ezequiel <eze.munoz@mi.unc.edu.ar>
2023-09-15 15:49:51 -0300

    entidad relación admin-usuarios y corrección validaciones

--- back/config/db-config.js
+++ back/config/db-config.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require('sequelize');
-// const { createUsersOnStart, createRolesOnStart } = require('../seeders/populateDB');
+const { createUsersOnStart, createRolesOnStart } = require('../seeders/populateDB');
 
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
@@ -14,11 +14,11 @@ const initializeDB = async () => {
     const Role = require('../models/role');
     const User = require('../models/user');
     console.log(sequelize.models);
-    await User.sync({ alter: false });
-    await Role.sync({ alter: false });
+    await Role.sync({ force: true });
+    await User.sync({ force: true });
 
-    // await createRolesOnStart();
-    // await createUsersOnStart();
+    await createRolesOnStart();
+    await createUsersOnStart();
   } catch (error) {
     console.error('Error al inicializar la base de datos', error);
   }
