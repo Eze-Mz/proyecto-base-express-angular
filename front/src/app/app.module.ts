d0790e91a9c682b3c94b27db53245ac7ebf17608
Pablo Medina <pablomedina2493@gmail.com>
2023-09-17 15:51:32 -0300

    Creación Header, Footer, Nav, Dashboard  Login e Inicio Sesion Funcional

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,16 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { NgApexchartsModule } from 'ng-apexcharts';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './auth/login/login.component';
+import { NavComponent } from './shared/nav/nav.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent,
+    LoginComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CoreModule,
-    NgApexchartsModule
+    NgApexchartsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
