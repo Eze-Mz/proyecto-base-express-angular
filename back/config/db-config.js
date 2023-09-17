6b82929b7e85e05e060e539f7087bb85dd275f50
Ezequiel <eze.munoz@mi.unc.edu.ar>
2023-09-17 20:50:24 -0300

    config mejorado

--- back/config/db-config.js
+++ back/config/db-config.js
@@ -1,5 +1,10 @@
 const { Sequelize } = require('sequelize');
 const { createUsersOnStart, createRolesOnStart } = require('../seeders/populateDB');
+const fs = require('fs');
+const path = require('path');
+const basename = path.basename(__filename);
+
+const populateDB = false;
 
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
@@ -7,19 +12,37 @@ const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, pr
   dialect: process.env.DB_DIALECT
 });
 
+const __parent = path.resolve(__dirname, '..');
+const models = path.join(__parent, 'models');
+
+const loadModels = () => {
+  fs
+    .readdirSync(models)
+    .filter(file => {
+      return (
+        file.indexOf('.') !== 0 &&
+      file !== basename &&
+      file.slice(-3) === '.js' &&
+      file.indexOf('.test.js') === -1
+      );
+    })
+    .forEach(file => {
+      const model = require(path.join(models, file));
+      sequelize[model.name] = model;
+    });
+};
+
 const initializeDB = async () => {
   try {
     await sequelize.authenticate();
     console.log('Conexión a la base de datos establecida');
-    const Role = require('../models/role');
-    const User = require('../models/user');
-    const Survey = require('../models/survey');
+    loadModels();
     console.log(sequelize.models);
-    await User.sync({ alter: false });
-    await Role.sync({ alter: false });
-    await Survey.sync({ alter: false });
-    await createRolesOnStart();
-    await createUsersOnStart();
+    if (populateDB) {
+      await sequelize.sync({ force: true });
+      await createRolesOnStart();
+      await createUsersOnStart();
+    }
   } catch (error) {
     console.error('Error al inicializar la base de datos', error);
   }
