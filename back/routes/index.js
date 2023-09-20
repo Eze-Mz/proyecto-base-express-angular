0907c481abf21d22887d6e2f21d56feba93832de
Ezequiel <eze.munoz@mi.unc.edu.ar>
2023-09-20 16:27:17 -0300

    endpoint para buscar respuestas

--- back/routes/index.js
+++ back/routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const userRouter = require('./user.js');
 const loginRouter = require('./login.js');
 const surveyRouter = require('./survey.js');
+const answerRouter = require('./answers.js');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -12,5 +13,6 @@ router.get('/', function (req, res, next) {
 router.use('/login', loginRouter);
 router.use('/user', userRouter);
 router.use('/survey', surveyRouter);
+router.use('/answer', answerRouter);
 
 module.exports = router;
--- back/services/index.js
+++ back/services/index.js
@@ -1,9 +1,9 @@
 const userService = require('./user.js');
 const surveyService = require('./survey.js');
-const edadService = require('./edad.js');
+const answerService = require('./answer.js');
 
 module.exports = {
   userService,
   surveyService,
-  edadService
+  answerService
 };
