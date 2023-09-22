b4e3866300eb4fbfc374988e09e43d252f21818f
Ezequiel <eze.munoz@mi.unc.edu.ar>
2023-09-22 15:07:47 -0300

    gráficos y plantilla tabla

--- front/src/app/pages/resultados/resultados.component.ts
+++ front/src/app/pages/resultados/resultados.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DatabaseService } from 'src/app/core/services/database.service';
 
 @Component({
   selector: 'app-resultados',
   templateUrl: './resultados.component.html',
   styleUrls: ['./resultados.component.css'],
 })
-export class ResultadosComponent {
-
+export class ResultadosComponent implements OnInit {
+  cantidadEncuestas = 0;
+  constructor(private db: DatabaseService) {}
+  ngOnInit(): void {
+    this.db.surveyCount().subscribe((count) => {
+      this.cantidadEncuestas = count;
+    }
+    );
+  }
 }
