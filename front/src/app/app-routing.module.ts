86f6a9eedbb87ab306c9c1977b3d2ca7c65cc33b
Pablo Medina <pablomedina2493@gmail.com>
2023-09-23 15:41:56 -0300

    Formulario arreglo ubicacion

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { ResultadosComponent } from './pages/resultados/resultados.component';
 import { adminGuard } from './core/guards/admin.guard';
 import { TablaEncuestasComponent } from './pages/tabla-encuestas/tabla-encuestas.component';
+import { CuestionarioTuristasComponent } from './pages/cuestionario-turistas/cuestionario-turistas.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'inicio', pathMatch:'full'},
@@ -14,6 +15,7 @@ const routes: Routes = [
   {path:'dashboard',component:DashboardComponent, canActivate:[loginGuard]},
   {path: 'dashboard/resultados', component: ResultadosComponent, canActivate:[adminGuard]},
   {path: 'dashboard/encuestas', component: TablaEncuestasComponent, canActivate:[loginGuard]},
+  {path:'dashboard/cuestionario-turista',component:CuestionarioTuristasComponent, canActivate:[loginGuard]},
   {path:'**',component: NotFoundComponent}
 ];
 
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -11,15 +11,12 @@ import { LoginComponent } from './auth/login/login.component';
 import { NavComponent } from './shared/nav/nav.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ChartsModule } from './modules/charts/charts.module';
-<<<<<<< HEAD
-import { CuestionarioTuristasComponent } from './shared/cuestionario-turistas/cuestionario-turistas.component';
+import { CuestionarioTuristasComponent } from './pages/cuestionario-turistas/cuestionario-turistas.component';
 
-=======
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { ResultadosComponent } from './pages/resultados/resultados.component';
 import { TablaEncuestasComponent } from './pages/tabla-encuestas/tabla-encuestas.component';
->>>>>>> f294bfe067e08de1fa0fd822160d6f16407d83d8
 
 
 @NgModule({
@@ -30,14 +27,11 @@ import { TablaEncuestasComponent } from './pages/tabla-encuestas/tabla-encuestas
     DashboardComponent,
     LoginComponent,
     NavComponent,
-<<<<<<< HEAD
-    CuestionarioTuristasComponent
-=======
+    CuestionarioTuristasComponent,
     HomeComponent,
     NotFoundComponent,
     ResultadosComponent,
     TablaEncuestasComponent,
->>>>>>> f294bfe067e08de1fa0fd822160d6f16407d83d8
   ],
   imports: [
     BrowserModule,
