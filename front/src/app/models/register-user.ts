cc1cb12798fa971841f2276cec78c0b4bb497b59
fullmendoza <fullstackmendoza@gmail.com>
2023-10-02 21:09:01 -0300

    se resolvio conflicto y agrego nombre y apellido en el register

--- front/src/app/models/register-user.ts
+++ front/src/app/models/register-user.ts
@@ -2,11 +2,15 @@ export class RegisterUser {
   getToken() {
     throw new Error('Method not implemented.');
   }
+  name: string;
+  lastName:string;
   email: string;
   password: string;
 
-  constructor(email: string | null, password: string | null) {
-      this.email = email ? email : '';
+  constructor(name: string | null,  lastName:string | null, email: string | null, password: string | null) {
+    this.name= name ? name : '';
+    this.lastName= lastName ? lastName: '';
+    this.email = email ? email : '';
       this.password = password ? password : '';
   }
 }
