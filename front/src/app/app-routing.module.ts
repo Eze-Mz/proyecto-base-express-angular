2fbde7b41feb12b4e88d5c4d7ec5e8a2cecbb69d
Pablo Medina <pablomedina2493@gmail.com>
2023-10-09 18:43:17 -0300

    Logo Favicon y Formulario de Contacto

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -3,19 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { loginGuard } from './core/guards/login.guard';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { SigninComponent } from './auth/signin/signin.component';
 
 
 const routes: Routes = [
   {path:'',redirectTo:'inicio', pathMatch:'full'},
   {path:'inicio', component:HomeComponent},
+
   {path:'dashboard', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule), canActivate:[loginGuard]},
-  {path:'sign-in', component:SigninComponent},
+
   {path:'**',component: NotFoundComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+
 })
 export class AppRoutingModule { }
